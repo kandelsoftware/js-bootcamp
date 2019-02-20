d9546880b9150dc1ba60fc0dfa3370d1214d6381
saroj <kandelsoftware@gmail.com>
2019-02-20 21:27:16 +0100

    functions

---
+++ functions-101.js
@@ -0,0 +1,20 @@
+//Function -input,code output
+let greetUser = function () {
+    console.log('welocme');
+};
+greetUser();
+
+let square = function (num) {
+    let result = num * num;
+    console.log(result);
+};
+
+square(3);
+square(10);
+
+fahrenheitTocelsius = function (fahernheit) {
+    let convertcelcius = (fahernheit - 32) * 5 / 9;
+    console.log(convertcelcius);
+
+};
+fahrenheitTocelsius(20);

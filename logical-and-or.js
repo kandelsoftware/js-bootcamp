22ad96f3e000a346e0318c341779c4d775ed11ee
saroj <kandelsoftware@gmail.com>
2019-02-20 20:38:24 +0100

    logic &&and ||

--- logical-and-or.js
+++ logical-and-or.js
@@ -7,3 +7,14 @@ if (temp >= 60 && temp <= 90) {
 } else {
     console.log("do what you like");
 }
+let isGuestOneVegan = true;
+let isGuestTwoVegan = false;
+
+
+if (isGuestOneVegan && isGuestTwoVegan) {
+    console.log("only serve vegan foods ")
+} else if (isGuestOneVegan || isGuestTwoVegan) {
+    console.log("server one vegan food ")
+} else {
+    console.log(" server from menu")
+}
